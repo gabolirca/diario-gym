979757da52e9d0f2684754a8e4d7c8892321a3e3
José Gabriel Lira Castelán <153787089+gabolirca@users.noreply.github.com>
2026-08-03 22:58:50 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: cachea la app para que funcione sin internet. */
-const CACHE = 'diario-gym-v1';
+const CACHE = 'diario-gym-v2';
 const FILES = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
